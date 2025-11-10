905a8ffaeb561aedfe6485a66bcf14d0a3df1d92
ANANDA-PC\ashik <ashik.cse.pstu@gmail.com>
2025-11-11 00:12:26 +0600

    update log

--- cypress/e2e/secondTest.cy.js
+++ cypress/e2e/secondTest.cy.js
@@ -1,7 +1,7 @@
 describe('Neflix Homepage', () => {
   it('should visit Netflix', () => {
     cy.visit('https://www.netflix.com')
-    cy.log('Test Two completed!')
+    cy.log('Test Two completed!!')
     cy.wait(1000)
   })
 })
